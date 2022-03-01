76aff4748f04e44584bd59df15e21484549402c9
CFLChrisz <100291263+CFLChrisz@users.noreply.github.com>
2022-03-01 09:35:34 -0500

    Initial commit

---
+++ HUH.py
@@ -0,0 +1,5 @@
+scar = "Frozone🥶🥶🥶"
+print(scar)
+#FreeSheisty
+pooh = "big BLRRRD!🐍🐍🐍"
+print(pooh)
---
+++ exercise1.py
@@ -0,0 +1,13 @@
+#Créer une fonction prenant en paramètre 
+#l'année de naissance d'une personne et 
+#lui retournant son l'âge qu'elle aura
+#à sa fête en 2022.
+
+def add_carre(n1=1, n2=1):
+ return n1**2 + n2**2
+
+n1 = 10
+n2 = 20
+print(add_carre(n1, n2))
+print(add_carre(n1))
+print(add_carre())
